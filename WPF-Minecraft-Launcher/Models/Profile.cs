ccf1e118801abafdd314f845019e72193ad6ccd1
Shark-vil <shark_vil@mail.ru>
2021-05-15 02:27:59 +0300

    Added player profile model

---
+++ WPF-Minecraft-Launcher/Models/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Minecraft_Launcher.Models
+{
+    public class Profile
+    {
+        internal string username;
+        internal string password;
+        internal string token;
+
+        public Profile(string username = "", string password = "", string token = "")
+        {
+            this.username = username;
+            this.password = password;
+            this.token = token;
+        }
+    }
+}
