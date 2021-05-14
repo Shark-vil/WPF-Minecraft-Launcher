0ef011f79fdc4781263fcb10f88f2ba4c9613f6e
Shark-vil <shark_vil@mail.ru>
2021-05-15 02:29:41 +0300

    added model AuthenticateModel

---
+++ WPF-Minecraft-Launcher/Models/Authenticate/AuthenticateModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Minecraft_Launcher.Models.Authenticate
+{
+    [Serializable]
+    public class AuthenticateModel
+    {
+        public string accessToken { get; set; }
+        public AuthenticateSelectedProfileModel selectedProfile { get; set; }
+
+        public static AuthenticateModel Convert(string json) => JsonConvert.DeserializeObject<AuthenticateModel>(json);
+    }
+
+    [Serializable]
+    public class AuthenticateSelectedProfileModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
